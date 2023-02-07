ace2ffbea966c1b3bb6c0f9e21278b8be52a3524
Dohwan <hwan@spadecompany.kr>
2023-02-07 11:04:24 +0900

    feat: commitlint boilerplate

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://spadecompany.net',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
