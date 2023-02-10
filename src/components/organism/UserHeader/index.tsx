20e5acce74f75f8fadf2d0946e99c9a196ca498a
Dohwan <hwan@spadecompany.kr>
2023-02-10 13:40:51 +0900

    feat: header 로고 스타일 간단하게 추가

--- src/components/organism/UserHeader/index.tsx
+++ src/components/organism/UserHeader/index.tsx
@@ -1,11 +1,20 @@
 import styles from './index.module.css';
-import Index from '@/components/molecule/LangChangeTab';
+import LangChangeTab from '@/components/molecule/LangChangeTab';
+import Link from 'next/link';
+import Image from 'next/image';
+import logoImage from '@/assets/img/logo.png';
 
 export default function UserHeader() {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
-      <Index />
+      <div className={`${styles.container} container`}>
+        <h1 className={styles.logo}>
+          <Link href={'/'}>
+            <Image src={logoImage} alt="Logo Image" />
+          </Link>
+        </h1>
+        <LangChangeTab />
+      </div>
     </header>
   );
 }
