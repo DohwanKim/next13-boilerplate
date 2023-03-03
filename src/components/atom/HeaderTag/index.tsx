8c51ca2fa788c1fbbdc0ef02fd191c54fac7de37
Dohwan <hwan@spadecompany.kr>
2023-03-03 13:23:12 +0900

    feat: 잡수정

--- src/components/atom/HeaderTag/index.tsx
+++ src/components/atom/HeaderTag/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+type headerLevel = 1 | 2 | 3 | 4 | 5 | 6;
+
 interface Props {
-  title: string | undefined;
-  type: number;
+  title?: string | undefined;
+  level?: headerLevel;
 }
 
-export default function HeaderTag({ title, type = 1 }: Props) {
-  return React.createElement(`h${type}`, {}, title);
+export default function HeaderTag({ title, level = 1 }: Props) {
+  return React.createElement(`h${level}`, {}, title || '');
 }
